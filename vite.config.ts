13b7bfe55338753c4ea26f341f27ee5f9b3072f9
mailong2401 <gtmailong@gmail.com>
2025-11-26 15:47:12 +0700

    .

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "./", // QUAN TRỌNG: Đổi thành relative path
+  base: "/",
   server: {
     host: "::",
     port: 8080,
